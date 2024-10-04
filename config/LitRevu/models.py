33a2a4fa99c85ce7374a5bf78d3d267734d5a0f0
antogro <153613140+antogro@users.noreply.github.com>
2024-10-04 10:17:46 +0200

    "Deleted entire Django project structure, including all apps, models, views, templates, and settings, creation of a new django project, with better organisation"

---
+++ config/LitRevu/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    followed_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
+    class Meta:
+        unique_together = ('user', 'followed_user')
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    image = models.ImageField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.headline} - {self.rating}/5"
+
+    
